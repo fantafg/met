621ce5234929e69bd35b46a3de419738e99b6d32
caioc123 <caiocavalcantedeoliveira14@hotmail.com>
2016-09-27 17:17:58 -0300

    first commit

---
+++ src/main/java/com/ifce/maven/academico_maven/App.java
@@ -0,0 +1,13 @@
+package com.ifce.maven.academico_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/ifce/maven/academico_maven/Calculadora.java
@@ -0,0 +1,12 @@
+package com.ifce.maven.academico_maven;
+
+public class Calculadora {
+
+	public int soma(int a, int b){
+		
+		return a+b;
+		
+	}
+	
+	
+}
