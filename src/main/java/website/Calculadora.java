2c1156911fa3556b05594f469487add26f5b5cd4
Alunos <Alunos>
2016-09-29 17:57:22 -0300

    projeto web

---
+++ src/main/java/website/Calculadora.java
@@ -0,0 +1,16 @@
+package website;
+
+public class Calculadora {
+	
+	public int soma(int a, int b){
+		
+		return a+b;
+		
+	}
+	
+	public int subtrair(int a, int b){
+		System.out.println("teste");
+		return a-b;
+		
+	}
+}
